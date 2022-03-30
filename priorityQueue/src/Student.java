45352497b806446950ff4df0d3c7ebed8c1df634
Igbekele Audu Desmond <auduigbekele@gmail.com>
2022-03-30 05:11:31 +0100

    Add files via upload
    PriorityQueue

---
+++ priorityQueue/src/Student.java
@@ -0,0 +1,35 @@
+public class Student {
+    private String name;
+    private int id;
+    private double cgpa;
+
+    public Student(String name, int id, double cgpa) {
+        this.name = name;
+        this.id = id;
+        this.cgpa = cgpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+
+    public void setCgpa(double cgpa) {
+        this.cgpa = cgpa;
+    }
+}
---
+++ priorityQueue/src/StudentPriorityQueue.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class StudentPriorityQueue {
+    private String ENTER;
+    private String SERVED;
+
+    List<Student>getStudent(List<String>events){
+        Student s= new Student("Greg",1,3.89);
+
+        return  null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
